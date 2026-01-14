286e799bb5ed36aeaa5b163e07efddf91ebf0ec6
siddgh123 <siddheshghadge528@gmail.com>
2026-01-14 23:09:57 +0530

    add

--- find_missing_from_array.java
+++ find_missing_from_array.java
@@ -4,16 +4,13 @@ class find_missing_from_array
     {
       int a[]={1,2,3,5};
       int sum=0,arr_sum=0;
+        int n=a.length+1;
       for(int i=0;i<a.length;i++)
       {
          arr_sum=arr_sum+a[i];
       }
-      for(int i=0;i<=a.length;i++)
-      {
-         sum=sum+(i+1);
-      }
-      System.out.println(sum);
-       int sum_arr=arr_sum-sum;
+      sum=n*(n+1)/2;
+       int sum_arr=sum-arr_sum;
          System.out.println("Missing number is: "+sum_arr);
 
     }
